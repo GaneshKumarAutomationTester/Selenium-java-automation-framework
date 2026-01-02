3569ed99097da2c2a6281f7f72922419a025066f
ganesh <ganeshkumarmuralidharan99@gmail.com>
2026-01-03 01:28:59 +0530

    committed by person 2

--- src/test/java/MavenLeaningdemo/mavenunderstand/appiumtest.java
+++ src/test/java/MavenLeaningdemo/mavenunderstand/appiumtest.java
@@ -9,6 +9,8 @@ public class appiumtest {
 	public void applogin()
 	{
 		System.out.println("applogin");
+		System.out.println("Person 2 commits for second time");
+		System.out.println("I am learning github ");
 	}
 	
 	@Test
